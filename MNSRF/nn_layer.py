aae8d736de219bfd9a613d879bb1ef311d28d9f8
QiannanCheng <2776294265@qq.com>
2019-04-20 21:08:23 +0800

    upload file

--- MNSRF/nn_layer.py
+++ MNSRF/nn_layer.py
@@ -175,26 +175,3 @@ class DecoderCell(nn.Module):
         ouput=f.log_softmax(self.out(output.squeeze(1)),1) #函数squeeze(arg): 表示第arg维的维度值为1，则去掉该维度，否则tensor不变
         return ouput,hidden
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
